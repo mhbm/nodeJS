f9ee9a566528f5fcf3a7f7d91956a2c087bda5f1
Mateus Macedo <mhbassomacedo@gmail.com>
2019-10-23 20:26:38 -0300

    Primeira versão
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

---
+++ config/custom-express.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+
+
+module.exports = function() {
+    var app = express();
+
+
+    app.use(bodyParser.urlencoded({ extend: true }));
+    app.use(bodyParser.json());
+
+
+
+
+    consign()
+        .include('controller')
+        .then('persistence')
+        .into(app);
+
+    return app;
+}
---
+++ controller/pagamentos.js
@@ -0,0 +1,54 @@
+const { check, validationResult } = require('express-validator');
+
+module.exports = function(app) {
+    app.get('/pagamentos', function(req, res) {
+        console.log("Recebido a requisição de teste.");
+        res.send('OK.');
+    });
+
+    app.post('/pagamentos/pagamento', [
+        // username must be an email
+        check('nome').not().isEmpty().withMessage('O campo nome é obrigatório'),
+        check('forma').not().isEmpty().withMessage('O campo forma é obrigatório')
+        .isLowercase("credito,debito").withMessage("O valor deve ser credito ou debito"),
+        // password must be at least 5 chars long
+        //  check('password').isLength({ min: 5 })
+        /*
+        check('valor')
+        .isLength({ min: 3 }).withMessage('must be at least 3 chars long')
+        .matches(/\d/).withMessage('must contain a number')
+            */
+        check('valor').not().isEmpty().withMessage('O campo valor é obrigatório')
+        .isCurrency().withMessage("It is not a valid currency amount"),
+
+    ], function(req, res) {
+        var teste = check('nome').isEmpty();
+        console.log(teste + "sss")
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        var pagamento = req.body;
+        console.log('processando uma requisicao de um novo pagamento');
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+
+        var connection = app.persistence.connectionFactory();
+        var pagamentoDao = new app.persistence.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, function(erro, resultado) {
+
+            if (erro) {
+                res.send(erro);
+            } else {
+                console.log('pagamento criado');
+                res.json(pagamento);
+            }
+
+
+        })
+
+    });
+}
